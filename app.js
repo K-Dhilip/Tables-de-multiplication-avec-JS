21d8f2d0a0419998c2d5f6f9c5c3ad52d502d8e7
Dhilip KARTHIKEYAN <74380649+K-Dhilip@users.noreply.github.com>
2022-03-18 21:12:52 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+for (i = 1; i <= 10; i++) {
+  table1.innerHTML += i + " x 2 " + "= " + i * 2 + "<br>";
+  table2.innerHTML += i + " x 3 " + "= " + i * 3 + "<br>";
+  table3.innerHTML += i + " x 4 " + "= " + i * 4 + "<br>";
+  table4.innerHTML += i + " x 5 " + "= " + i * 5 + "<br>";
+  table5.innerHTML += i + " x 6 " + "= " + i * 6 + "<br>";
+  table6.innerHTML += i + " x 7 " + "= " + i * 7 + "<br>";
+  table7.innerHTML += i + " x 8 " + "= " + i * 8 + "<br>";
+  table8.innerHTML += i + " x 9 " + "= " + i * 9 + "<br>";
+  table9.innerHTML += i + " x 10 " + "= " + i * 10 + "<br>";
+  table10.innerHTML += i + " x 11 " + "= " + i * 11 + "<br>";
+  table11.innerHTML += i + " x 12 " + "= " + i * 12 + "<br>";
+  table12.innerHTML += i + " x 13 " + "= " + i * 13 + "<br>";
+}
